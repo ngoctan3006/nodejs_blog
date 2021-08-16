858202130623d7caf4d4f5660a2668e743e35b04
Tân <ngoctana1k2014@gmail.com>
2021-08-17 03:21:28 +0700

    update file

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+// HTTP logger
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
